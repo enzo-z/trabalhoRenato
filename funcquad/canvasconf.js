4c7e1f95840dbd2a5c1d418c3725242634b03dea
enzo-z <ezamoradev@gmail.com>
2019-07-09 20:07:39 -0300

    Polida no código do funcquad, e terminado o css dele. FALTA REVISAO NA FUNCAO drawParabola()

--- funcquad/canvasconf.js
+++ funcquad/canvasconf.js
@@ -1,37 +1,42 @@
 canvasElement.width = 400;
 canvasElement.height = 400;
 
-
 var c = canvasElement.getContext("2d");  //Setando canvas pra 2d
 c.height = canvasElement.height;
 c.width = canvasElement.width;
-var intervalo = c.height/10; 
-
+var intervalo = c.height/10; //<<--- [REVISAR ISTO AQUI!] 
 
 function constroiCanvas(){
-    c.clearRect(0, 0, c.width, c.height);
+    c.clearRect(0, 0, c.width, c.height); //Atualizar canvas
 
+    //Desenhando o plano cartesiano
+    //Eixo x
     c.beginPath();
     c.moveTo(0, c.height/2);
     c.lineTo(c.width, c.height/2);
     c.stroke();
+    c.closePath();
+
+    //Eixo y
     c.beginPath();
     c.moveTo(c.width/2, c.height);
-    c.lineTo(c.width/2, -c.height);
-    c.strokeStyle = "#000000";
+    c.lineTo(c.width/2, 0);
     c.stroke();
-    numeraLinha(c.width/2, c.height/2);
+    c.closePath();
 
-
-    //Test
-    c
+    //Bolinha vermelha no eixo(0,0)
     c.beginPath();
     c.arc(c.width/2, c.height/2, 3, 0, Math.PI *2, false);
     c.fillStyle = "rgba(255, 0, 0, 1)";
     c.fill();
-    //
+    
+    //Criando os tracinhos nos eixos. VER SE CONSIGO ADICIONAR NÚMEROS DEPOIS!
+    numeraLinha(c.width/2, c.height/2);
+
+
+
 }
-    //Função numera os pontos
+
 function numeraLinha(linhaX, linhaY){
     console.log(intervalo);
     for(let i = 0; i<=10; i++){
@@ -39,28 +44,25 @@ function numeraLinha(linhaX, linhaY){
         c.moveTo(i*intervalo , linhaY +10);
         c.lineTo(i*intervalo , linhaY -10 );
         c.stroke();
+        c.closePath();
     }
     for(let i = 0; i<=10; i++){
         c.beginPath();
         c.moveTo(linhaX+10, i*intervalo);
         c.lineTo(linhaX-10 ,i*intervalo);
         c.stroke();
+        c.closePath();
     }
-
-
 }
 
 function drawRoots(r1, r2){
     c.beginPath();
     console.log("DENTRO DO DRAW ROOTS: " +r1);
-    //c.moveTo(c.width/2, c.height/2);
     c.fillStyle = "rgba(255, 255, 0, 1)";
     c.arc(c.width/2+(r1*intervalo), c.height/2, 3, 0, Math.PI *2, false);
     c.arc(c.width/2+(r2*intervalo), c.height/2, 3, 0, Math.PI *2, false);
     c.fill();
     c.closePath();
-    
-
 
 }
 
@@ -71,11 +73,10 @@ function drawVert(vx, vy){
     c.arc(c.width/2 + (vx * intervalo), (c.height/2)-(vy*intervalo), 3, 0, Math.PI*2, false);
     c.fill();
     c.closePath();
-
 }
 
 
-function drawParabola(r1, r2, vx, vy, delta, numeros){ //ALERTA DE GAMBIARRA
+function drawParabola(r1, r2, vx, vy, delta, numeros){ //ALERTA DE GAMBIARRA PRO ENZO DO FUTURO!
     c.beginPath();
 
     if (delta == 0){ //Gambiarríssima [VERIFICAR URGENTEMENTE]
@@ -92,7 +93,7 @@ function drawParabola(r1, r2, vx, vy, delta, numeros){ //ALERTA DE GAMBIARRA
 
     }
 
-    //Se tem duas raizes reais: (Não é gambiarra)
+    //Se tem duas raizes reais: (Não é gambiarra), MAS BOM REVISAR
     if (delta > 0){
     c.moveTo(c.width/2 + (r1 * intervalo), c.height/2);
     c.quadraticCurveTo(c.width/2 + (vx * intervalo), (c.height/2)-vy*intervalo*2, c.width/2 + (r2*intervalo), c.height/2);    
@@ -116,7 +117,6 @@ function drawParabola(r1, r2, vx, vy, delta, numeros){ //ALERTA DE GAMBIARRA
 
 
     }
-
     c.closePath();
 }
 
--- funcquad/mainquad.js
+++ funcquad/mainquad.js
@@ -2,72 +2,67 @@
 //JS da função quadrática [Calculos]
 var btnElement = document.getElementById("btn");
 var canvasElement = document.getElementById("canvas");
-var raizesElement = document.getElementById("roots");
-var vertElement = document.getElementById("vertices");
-
+var root1Element = document.getElementById("root1");
+var root2Element = document.getElementById("root2");
+var ver1Element = document.getElementById("vertex1");
+var ver2Element = document.getElementById("vertex2");
 btnElement.onclick = function(){
-    
     funcQuad(getNumeros());
- 
-
 }
 
 
 
 function funcQuad(numeros){ //Numeros[0] == a, numeros[1] == b, numeros[2] == c!
-    console.log("Entrou aqui");
     if (numeros[0] == undefined || numeros[1] == undefined || numeros[2] == undefined){
         return;
     }
-    console.log("Dentro do funcQuad: "+ numeros[0], numeros[1], numeros[2]);
     var delta = Math.pow(numeros[1], 2) - (4 * numeros[0] * numeros[2]);
     var r1, r2; // Raizes
     var vx, vy; // vertices
     if (delta < 0){
-        raizesElement.textContent = "It has no real roots!";
+        root1Element.value = "It has no real root!";
+        root2Element.value = "It has no real root!";
+
 
     }
     if (delta == 0){
         r1 = ((-numeros[1]) + Math.sqrt(delta))/ 2 * numeros[0];
-        raizesElement.textContent =  "Only one root: "+ r1;
+        root1Element.value = "Same root as r2: "+r1;
+        root2Element.value = "Same root as r1: "+r1;
     }
-    
     if(delta > 0){
         r1 = (-numeros[1] + Math.pow(delta, 0.5)) / (2 * numeros[0]);
         r2 = ((-numeros[1]) - Math.sqrt(delta))/ (2 * numeros[0]);
-        raizesElement.textContent = "Duas raízes! R1: "+r1+" e R2: "+r2;
+        root1Element.value = "Root 1: "+r1;
+        root2Element.value = "Root 2:"+ r2;
     }
-    
     vy = -delta / (4*numeros[0]);
     vx =  -numeros[1] / (2*numeros[0]); 
+    //Teste dos vertices
     console.log("VERTICES:"+ vx, vy);
+    //Teste das raízes
+    console.log("SUAS RAIZES:" +r1, r2)
 
-    vertElement.textContent = "Vertice X: "+vx+"Vertice Y: "+vy;
+    ver1Element.value = "Vertex X: "+vx;
+    ver2Element.value = "Vertex Y: "+vy;
+    
 
-    //Canvas
+    //canvasconf
 
     constroiCanvas();
-    console.log("SUAS RAIZES:" +r1, r2)
     drawRoots(r1, r2);
     drawVert(vx, vy);
-    drawParabola(r1, r2, vx, vy, delta, numeros);
-
-
-    
-    
-
+    drawParabola(r1, r2, vx, vy, delta, numeros); //NECESSITA DE REVISÃO!
 
 }
 
 
 function getNumeros(){
-    console.log("Entrou no getNumeros");
     if( document.getElementById("a").value % 1 != 0 || document.getElementById("b").value %1 != 0 || 
     document.getElementById("c").value % 1 != 0){
         alert("ONLY INTEGERS ARE ACCEPTED!");
 
     } 
-    
     else{ 
         var numeros = new Array(3);
         numeros[0] = parseInt(document.getElementById("a").value);
